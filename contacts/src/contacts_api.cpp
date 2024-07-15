9f58bd2e6cf44618f33930d431585343ccdd1371
majshr <2586091503@qq.com>
2024-07-15 14:28:10 +0000

    update contacts/src/contacts_api.cpp.
    Signed-off-by: majshr <2586091503@qq.com>

--- contacts/src/contacts_api.cpp
+++ contacts/src/contacts_api.cpp
@@ -952,8 +952,8 @@ void LocalExecuteIsMyCard(napi_env env, ExecuteHelper *executeHelper)
 void LocalExecute(napi_env env, ExecuteHelper *executeHelper)
 {
     if (executeHelper->dataShareHelper == nullptr) {
-		HILOG_ERROR("create dataShareHelper is null, please check your permission");
-		executeHelper->resultData = RDB_PERMISSION_ERROR;
+	    HILOG_ERROR("create dataShareHelper is null, please check your permission");
+	    executeHelper->resultData = RDB_PERMISSION_ERROR;
         return;
     }
     switch (executeHelper->actionCode) {
@@ -963,11 +963,11 @@ void LocalExecute(napi_env env, ExecuteHelper *executeHelper)
         case DELETE_CONTACT:
             LocalExecuteDeleteContact(env, executeHelper);
             break;
-		case UPDATE_CONTACT:
+	    case UPDATE_CONTACT:
             LocalExecuteUpdateContact(env, executeHelper);
             break;
         default:
-			LocalExecuteSplit(env, executeHelper);
+		    LocalExecuteSplit(env, executeHelper);
             HILOG_INFO("LocalExecute case error===>");
             break;
     }
@@ -975,14 +975,14 @@ void LocalExecute(napi_env env, ExecuteHelper *executeHelper)
 
 void LocalExecuteSplit(napi_env env, ExecuteHelper *executeHelper)
 {
-	ContactsTelephonyPermission permission;
-	if(!permission.CheckPermission(ContactApi::Permission::READ_CONTACTS)){
-		HILOG_ERROR("LocalExecuteQueryContactsByData Permission denied!");
-		executeHelper->resultData = RDB_PERMISSION_ERROR;
+    ContactsTelephonyPermission permission;
+    if(!permission.CheckPermission(ContactApi::Permission::READ_CONTACTS)){
+        HILOG_ERROR("LocalExecuteQueryContactsByData Permission denied!");
+        executeHelper->resultData = RDB_PERMISSION_ERROR;
         return;
 	}else if(executeHelper->resultData == VERIFICATION_PARAMETER_ERROR){
-		HILOG_ERROR("PARAMETER_ERROR, please check your PARAMETER");
-		return;
+	    HILOG_ERROR("PARAMETER_ERROR, please check your PARAMETER");
+	    return;
 	}
 	switch (executeHelper->actionCode) {
         case QUERY_CONTACT:
